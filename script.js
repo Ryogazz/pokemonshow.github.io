eacdda3731cdff112cc645ca13413c63fa862bca
Ryogazz <josuex.x.x@hotmail.com>
2021-10-26 18:27:29 -0300

    melhorando

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const botaoCorreto = async () => {
   botao.addEventListener('click', () =>{
     botao.classList.add('botao-correto');
     pokeImg.src = resultado.sprites.front_default;
-    setTimeout(() => alert('PARABÉNS!!!!!!!!!!'),1000)
+    setTimeout(() => alert('PARABÉgitNS!!!!!!!!!!'),1000)
     setTimeout(() => location.reload(),4000)
   })
 }
